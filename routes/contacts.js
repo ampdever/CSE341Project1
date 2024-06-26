938b31cfcd672e3ab310352d87e2fbccb5b9fa6a
ampdever <Mag23019@byui.edu>
2024-06-25 21:24:21 -0600

    finished contacts 2 and installed swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,8 @@ const contactsController = require('../controllers/contacts');
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
 
+router.post('/', contactsController.createContact); //create
+router.put('/:id', contactsController.updateContact); //update
+router.delete('/:id', contactsController.deleteContact); //delete
+
 module.exports = router;
